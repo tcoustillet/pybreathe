fa4b2e0a63adc788c678f04efb5d539e7074bdbb
Thibaut Coustillet <thib.coustillet@gmail.com>
2025-04-02 17:41:15 +0200

    interpolation of 'True' zeros and signal truncation on object instantiation

--- pybreathe/breathingflow.py
+++ pybreathe/breathingflow.py
@@ -26,16 +26,16 @@ class BreathingFlow:
             .reset_index(drop=True)
         data = data.apply(lambda col: col.str.replace(",", ".").astype(float))
 
-        self.absolute_time = data["time"].values
+        self.raw_time = data["time"].values
         self.raw_flow = data["values"].values
 
-        time_len = len(self.absolute_time)
-        self.time = (
+        time_len = len(self.raw_time)
+        self.absolute_time = (
             np.linspace(0, time_len / self.get_hz(), time_len, endpoint=False)
         )
 
-        detrended_flow = detrend(self.raw_flow)
-        self.flow = detrended_flow
+        self.detrended_flow = detrend(self.raw_flow)
+        self.__zero_interpolation()
 
     def get_hz(self):
         """
@@ -46,18 +46,66 @@ class BreathingFlow:
             int: the sampling rate in Hz (s-1).
 
         """
-        time_delta = pd.to_timedelta(self.absolute_time, unit="s")
+        time_delta = pd.to_timedelta(self.raw_time, unit="s")
         diff = time_delta.diff().value_counts().index.tolist()[0]
 
         return int(pd.Timedelta(seconds=1) / diff)
 
-    def plot(self, y=None):
+    def __zero_interpolation(self):
+        """To find the 'True' zeros of a discretized signal."""
+        x, y = self.absolute_time, self.detrended_flow
+
+        crossing_indices = np.where(
+            np.array(
+                [0 if e in (-1, 1) else e for e in np.diff(np.sign(y))]
+            )
+        )[0]
+
+        x_zeros, y_zeros = [], []
+        for i in crossing_indices:
+            x_upstream, x_downstream = x[i], x[i + 1]
+            y_upstream, y_downstream = y[i], y[i + 1]
+
+            zero = x_upstream - y_upstream * (x_downstream - x_upstream) / (
+                y_downstream - y_upstream
+            )
+            x_zeros.append(zero)
+            y_zeros.append(0.0)
+
+        x_extended = np.concatenate((x, x_zeros))
+        y_extended = np.concatenate((y, y_zeros))
+
+        sorted_indices = np.argsort(x_extended)
+        x_interpolated = x_extended[sorted_indices]
+        y_interpolated = y_extended[sorted_indices]
+
+        # Truncation to start and end with a 'True' zero.
+        first_zero = np.where(y_interpolated == 0)[0][0]
+        lasy_zero = np.where(y_interpolated == 0)[0][-1] + 1
+
+        x_truncated = x_interpolated[first_zero:lasy_zero]
+        y_truncated = y_interpolated[first_zero:lasy_zero]
+
+        self.time = x_truncated
+        self.flow = y_truncated
+
+    def plot(self, y="flow"):
         """To plot the air flow rate."""
         fig, ax = plt.subplots(figsize=(12, 2))
 
-        flow = self.flow if y is None else self.raw_flow
+        match y:
+            case "flow":
+                x, y = self.time, self.flow
+            case "raw_flow":
+                x, y = self.absolute_time, self.raw_flow
+            case "detrended_flow":
+                x, y = self.absolute_time, self.detrended_flow
+            case _:
+                raise AttributeError(
+                    f"{self.__class__.__name__} object has no attribute '{y}'"
+                )
 
-        ax.plot(self.time, flow, label="air flow rate")
+        ax.plot(x, y, label="air flow rate")
         ax.set_xlabel("time (s)", labelpad=10)
         ax.set_ylabel("Air flow rate", labelpad=10)
         ax.legend()
