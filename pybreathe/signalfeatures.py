012d63efde1d2eb80fb7cbe738495174abd62383
Thibaut Coustillet <thib.coustillet@gmail.com>
2025-04-03 15:13:50 +0200

    positive and negative segments start and end with 0 (phase change moment)

--- pybreathe/signalfeatures.py
+++ pybreathe/signalfeatures.py
@@ -8,6 +8,9 @@ Created on Thu Apr  3 10:10:59 2025
 """
 
 
+import numpy as np
+
+
 def get_segments(x, y):
     """
     To get positive and negative segments of a signal.
@@ -25,37 +28,72 @@ def get_segments(x, y):
     """
     positive_segments, negative_segments = [], []
     pos_x, pos_y, neg_x, neg_y = [], [], [], []
-    
+
     for i in range(len(y)):
         if y[i] > 0:
             if len(neg_x) > 0:
                 negative_segments.append((neg_x, neg_y))
                 neg_x, neg_y = [], []
-            
+
             pos_x.append(x[i])
             pos_y.append(y[i])
-        
+
         elif y[i] < 0:
             if len(pos_x) > 0:
                 positive_segments.append((pos_x, pos_y))
                 pos_x, pos_y = [], []
-            
+
             neg_x.append(x[i])
             neg_y.append(y[i])
-            
+
         else:
             if len(pos_x) > 0:
                 positive_segments.append((pos_x, pos_y))
                 pos_x, pos_y = [], []
-            
+
             if len(neg_x) > 0:
                 negative_segments.append((neg_x, neg_y))
                 neg_x, neg_y = [], []
-    
+
     if len(pos_x) > 0:
         positive_segments.append((pos_x, pos_y))
-    
+
     if len(neg_x) > 0:
         negative_segments.append((neg_x, neg_y))
-    
+
+    def add_zeros(x, y, segments):
+        """
+        To make the positive and negative segments begin and end with 0.
+
+        Args:
+        ----
+            x (array): a discretised time in seconds (0.001, 0.002, 0.003, ...).
+            y (array): a discretized breathing air flow rate.
+            segments (list): list of arrays where the values of y have always the same sign.
+
+        Returns:
+        -------
+            segments (array): pairs (x, y) where y begins and ends with a 0.
+
+        """
+        for s in segments:
+            first_point = s[0][0]
+            last_point= s[0][-1]
+
+            first_xzero = x[(np.where(x == first_point)[0][0] - 1)]
+            last_xzero = x[(np.where(x == last_point)[0][0] + 1)]
+
+            first_yzero = y[(np.where(x == first_point)[0][0] - 1)]
+            last_yzero = y[(np.where(x == last_point)[0][0] + 1)]
+
+            s[0].insert(0, first_xzero)
+            s[0].append(last_xzero)
+            s[1].insert(0, first_yzero)  # supposed to be 0.
+            s[1].append(last_yzero)  # supposed to be 0.
+
+        return segments
+
+    positive_segments = add_zeros(x, y, positive_segments)
+    negative_segments = add_zeros(x, y, negative_segments)
+
     return positive_segments, negative_segments
