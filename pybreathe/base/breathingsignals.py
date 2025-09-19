7ae275ea6fd66e34e6a4b29c7ffbe3cf9c3ab236
Thibaut Coustillet <thib.coustillet@gmail.com>
2025-09-19 15:50:22 +0200

    fix(get_info): pass the argument for the shape of the function output (dict or df)

--- pybreathe/base/breathingsignals.py
+++ pybreathe/base/breathingsignals.py
@@ -65,7 +65,7 @@ class BreathingSignals(ComparableMixin):
     @enforce_type_arg(shape=str)
     def get_info(self, shape="dict"):
         """To get signal information (identifier, start, end and duration.)"""
-        return self.thorax.get_info()
+        return self.thorax.get_info(shape=shape)
 
     @enforce_type_arg(overlay=bool, output_path=str)
     def plot(self, overlay=False, output_path=""):
